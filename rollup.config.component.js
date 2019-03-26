79f708d13198251de1c54735bf8abe824fcd4f4a
Adam Shaw <arshaw@users.noreply.github.com>
2019-03-26 01:16:22 -0500

    tweaks

--- rollup.config.component.js
+++ rollup.config.component.js
@@ -1,9 +1,10 @@
 import vue from 'rollup-plugin-vue' // Handle .vue SFC files
 import buble from 'rollup-plugin-buble' // Transpile/polyfill with reasonable browser support
-import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
-const UMD_GLOBALS = { // get this from core project somehow?
+// https://vuejs.org/v2/cookbook/packaging-sfc-for-npm.html
+
+const UMD_GLOBALS = {
   '@fullcalendar/core': 'FullCalendar'
 }
 
@@ -21,14 +22,8 @@ export default {
     resolve({
       jail: 'src' // TODO: use this in core project
     }),
-    commonjs({ // needed for demo!!! for default export or something
-      namedExports: {
-        'node_modules/@fullcalendar/core/main.js': [ 'Calendar' ] // ahhhhhh
-      }
-    }),
     vue({
-      css: true, // Dynamically inject css as a <style> tag
-      compileTemplate: true, // Explicitly convert template to render function
+      compileTemplate: true // Explicitly convert template to render function
     }),
     buble({ // Transpile to ES5
       transforms: {
