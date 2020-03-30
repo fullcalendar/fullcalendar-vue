13578f5a331c73e80b7d5da431c650997ae9e56f
Adam Shaw <arshaw@users.noreply.github.com>
2020-03-30 18:14:05 -0400

    cleanup v5 code changes

--- src/FullCalendar.js
+++ src/FullCalendar.js
@@ -1,76 +1,85 @@
 import { Calendar } from '@fullcalendar/core'
 import { COMPLEX_OPTIONS } from './fullcalendar-options'
+import { shallowCopy, diffProps } from './utils'
 
 
 export default {
   props: [ 'options' ],
+
   data() {
     return {
       renderId: 0
     }
   },
+
   render(createElement) {
     return createElement('div', {
       // when renderId is changed, Vue will trigger a real-DOM async rerender, calling beforeUpdate/updated
       attrs: { 'data-fc-render-id': this.renderId }
     })
   },
+
   mounted() {
     this.$options.calendar = new Calendar(this.$el, this.options)
     this.$options.calendar.render()
   },
+
   methods: {
     getApi() {
       return this.$options.calendar
     }
   },
+
   beforeUpdate() {
-    this.getApi().resumeRendering() // the watcher paused it
+    this.getApi().resumeRendering() // the watcher handlers paused it
   },
+
   beforeDestroy() {
     this.getApi().destroy()
   },
+
   watch: buildWatchers()
 }
 
 
 function buildWatchers() {
   let watchers = {
+    // the root options handler. won't get fired when nested objects are mutated but root is not
     options(options, oldOptions) {
-      let calendar = this.getApi()
-      calendar.pauseRendering()
-      calendar.resetOptions(options, oldOptions)
-      this.renderId++
+      let diff = diffProps(oldOptions, options)
+
+      if (diff.anyChanges) {
+        let calendar = this.getApi()
+        calendar.pauseRendering()
+        calendar.mutateOptions(diff.updates, diff.removals)
+
+        this.renderId++ // will queue a rerender
+      }
     }
   }
 
   for (let complexOptionName of COMPLEX_OPTIONS) {
+
+    // handlers called when nested objects change
     watchers[`options.${complexOptionName}`] = {
       deep: true,
       handler(val) {
-        if (val !== undefined) { // when option is not specified in an update, given as undefined
+        // unfortunately the handler is called with undefined if new props were set, but the complex one wasn't ever set
+        if (val !== undefined) {
+
           let calendar = this.getApi()
           calendar.pauseRendering()
-          this.getApi().mutateOptions({
-            [complexOptionName]: copyTopLevel(val) // the copy will cache-bust internal purecomponents
+          calendar.mutateOptions({
+            // the only reason we shallow-copy is to trick FC into knowing there's a change when nested values change but the reference doesn't.
+            // TODO: future versions of FC will more gracefully handle event option-changes that are same-reference.
+            [complexOptionName]: shallowCopy(val)
           })
-          this.renderId++
+
+          this.renderId++ // will queue a rerender
         }
       }
     }
   }
 
   return watchers
-}
-
-
-function copyTopLevel(val) {
-  if (typeof val === 'object') {
-    if (Array.isArray(val)) {
-      val = Array.prototype.slice.call(val)
-    } else if (val) { // non-null
-      val = { ...val }
-    }
-  }
-  return val
 }
---
+++ src/utils.js
@@ -0,0 +1,38 @@
+
+/*
+works with objects and arrays
+*/
+export function shallowCopy(val) {
+  if (typeof val === 'object') {
+    if (Array.isArray(val)) {
+      val = Array.prototype.slice.call(val)
+    } else if (val) { // non-null
+      val = { ...val }
+    }
+  }
+
+  return val
+}
+
+
+export function diffProps(oldProps, newProps) {
+  let updates = {}
+  let removals = []
+  let anyChanges = false
+
+  for (let propName in newProps) {
+    if (newProps[propName] !== oldProps[propName]) {
+      updates[propName] = newProps[propName]
+      anyChanges = true
+    }
+  }
+
+  for (let propName in oldProps) {
+    if (!(propName in newProps)) {
+      removals.push(propName)
+      anyChanges = true
+    }
+  }
+
+  return { updates, removals, anyChanges }
+}
