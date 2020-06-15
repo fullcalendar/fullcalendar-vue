9194e1adfb6ee74d375a704d96f00f3da1c16218
Adam Shaw <arshaw@users.noreply.github.com>
2020-06-15 13:56:52 -0400

    move webpack into karma

--- karma.conf.js
+++ karma.conf.js
@@ -4,10 +4,36 @@ module.exports = function(config) {
     basePath: '.',
     frameworks: [ 'jasmine' ],
     files: [
-      'tmp/tests.js'
+      'tests/main.js'
     ],
     preprocessors: {
-      '**/*.+(js|css)': [ 'sourcemap' ]
+      'tests/main.js': [ 'webpack', 'sourcemap' ]
+    },
+    webpack: {
+      mode: 'development',
+      devtool: 'inline-source-map', // TODO: only process for our files???
+      resolve: {
+        extensions: [ '.js' ]
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [ '@babel/preset-env' ],
+                plugins: [ '@babel/plugin-transform-runtime' ] // for async keyword
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+          }
+        ]
+      }
     },
     logLevel: config.LOG_INFO,
     reporters: [ 'spec' ]
