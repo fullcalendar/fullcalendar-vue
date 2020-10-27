8f4e6550bea24ce234f265d353be33522843d70b
Adam Shaw <arshaw@users.noreply.github.com>
2020-10-27 18:07:15 -0400

    stop using karma-webpack

--- karma.config.cjs
+++ karma.config.cjs
@@ -5,47 +5,11 @@ module.exports = function(config) {
     basePath: '.',
     frameworks: [ 'jasmine' ],
     files: [
-      'tests/main.js'
+      'tmp/compiled-tests/main.js',
+      'tmp/compiled-tests/*.js'
     ],
     preprocessors: {
-      'tests/main.js': [ 'webpack', 'sourcemap' ]
-    },
-    webpack: {
-      mode: 'development',
-      devtool: 'inline-source-map', // output an inline sourcemap. gets fed into karma-sourcemap-loader
-      resolve: {
-        extensions: [ '.js' ]
-      },
-      module: {
-        rules: [
-          {
-            test: /\.vue$/,
-            use: 'vue-loader'
-          },
-          {
-            test: /\.js$/, // for fullcalendar lib files. TODO: exclude @vue/test-utils
-            use: 'source-map-loader'
-          },
-          {
-            test: /[\/]tests[\/][^.]*\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: [ '@babel/preset-env' ],
-                plugins: [ '@babel/plugin-transform-runtime' ] // for async keyword
-              }
-            }
-          },
-          {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-          }
-        ]
-      },
-      plugins: [
-        new VueLoaderPlugin()
-      ]
+      'tmp/compiled-tests/*.js': [ 'sourcemap' ]
     },
     logLevel: config.LOG_INFO,
     reporters: [ 'spec' ]
