c65bc3d581af2ac50f8bb44453ce72054967ed63
Adam Shaw <arshaw@users.noreply.github.com>
2020-11-05 15:35:09 -0500

    remove webpack hack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,12 +11,6 @@ module.exports = (env) => ({
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        resolve: {
-          fullySpecified: false // solves annoying errors about extensions. REVISIT
-        }
-      },
-      {
         test: /\.vue$/,
         use: 'vue-loader'
       },
