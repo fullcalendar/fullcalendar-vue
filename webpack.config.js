2d0368c6b029837da939cd4ced916dd10f6a1a93
Adam Shaw <arshaw@users.noreply.github.com>
2021-05-28 11:10:38 -0400

    upgrade build env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = (env) => ({
   mode: 'development',
