86feaddad362080995bb8a1ee46eb0bcae7b06e5
Adam Shaw <arshaw@users.noreply.github.com>
2021-05-31 12:26:28 -0400

    use rollup plugins better

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,8 +1,8 @@
 const { fromRollup } = require('@web/dev-server-rollup')
-const postcss = fromRollup(require('rollup-plugin-postcss'))
+const alias = fromRollup(require('@rollup/plugin-alias'))
+const replace = fromRollup(require('@rollup/plugin-replace'))
 const vue = fromRollup(require('rollup-plugin-vue'))
-const replace = fromRollup(require('rollup-plugin-replace'))
-const alias = fromRollup(require('@rollup/plugin-alias')) // TODO: have other plugins use new
+const postcss = fromRollup(require('rollup-plugin-postcss'))
 
 module.exports = {
   nodeResolve: true,
@@ -14,16 +14,29 @@ module.exports = {
     '**/*.css': 'js', // tell the server to serve css files as js
   },
   plugins: [
-    postcss({ inject: true }), // js will inject styles into page
     alias({
       entries: {
-        'vue': 'vue/dist/vue.esm-bundler.js' // works? why still the warning about esm-bundler
+        // use the Vue build that does runtime compilation of templates
+        'vue': 'vue/dist/vue.esm-bundler.js',
       }
     }),
-    vue(),
-    replace({ // needed anymore?
+    replace({
       'process.env.NODE_ENV': JSON.stringify('development'),
-      'process.env.VUE_ENV': JSON.stringify('browser')
-    })
+      'process.env.VUE_ENV': JSON.stringify('browser'),
+
+      // recommended build flags. also, when defined, quiets template warning
+      // https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+
+      // build flags for i18n plugin. doesn't quiet all warnings unfortunately
+      __VUE_I18N_FULL_INSTALL__: true,
+      __VUE_I18N_LEGACY_API__: true, // TODO: flip off!!! fix!!!
+      __VUE_I18N_PROD_DEVTOOLS__: false,
+    }),
+    postcss({
+      inject: true, // js will inject styles into page
+    }),
+    vue(),
   ],
 }
